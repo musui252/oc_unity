49ffddef77bd5328a36851ebe22dc14c6660aea9
musui252 <symphogear252@gmail.com>
2021-07-14 18:38:14 +0900

    最初のコミット
    ボールと手

---
+++ Assets/MyScript/KickBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickBall : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.B))
+        {
+            rb.AddForce(transform.forward * speed);
+        }
+       
+    }
+}
